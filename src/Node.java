afbbf38d81a779f779113c5d90d98984b8f60a22
YannisFalco <yannis.falco@etu.unice.fr>
2019-10-08 12:45:30 +0100

    Creation of node and tree structure #1

---
+++ src/Node.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    private List<Node> nodesSon;
+
+    Node(){
+        nodesSon = new ArrayList<>();
+    }
+
+    void addSon(Node node){
+        nodesSon.add(node);
+    }
+
+    List<Node> getSons() {
+        return new ArrayList<>(nodesSon);
+    }
+}
