6fda12b35e5eb98386b3dd0625ee2e45ec6a2b3e
YannisFalco <yannis.falco@etu.unice.fr>
2019-10-15 17:56:44 +0100

    First test for tree #1

--- test/TreeTest.java
+++ test/TreeTest.java
@@ -4,5 +4,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TreeTest {
-    
+    Tree t1;
+
+    @Before
+    public void initialize(){
+        Node node = new Node(0);
+        t1 = new Tree(node);
+        for(int i = 1; i<10; i++){
+            Node n = new Node(i);
+            node.addSon(n);
+        }
+
+    }
+
+    @Test
+    public void output() {
+        String s1 = t1.output();
+        System.out.println(s1);
+        assertTrue(t1.output().equals("(0(1-2-3-4-5-6-7-8-9-))"));
+
+    }
 }
