f3d9f718958c65787733f2fd188db588d25f39a0
YannisFalco <yannis.falco@etu.unice.fr>
2019-10-16 21:44:25 +0100

    corrected lca v1 Fixes #4 Fixes #5

--- src/Lca.java
+++ src/Lca.java
@@ -18,15 +18,20 @@ public class Lca {
 
     Node recursiveLCAFromValue(Node node, int node1, int node2){
         List<Node> nodeSon = new ArrayList<>(node.getSons());
-        int countTrue = 0;
         if(node.getValue() == node1 || node.getValue() == node2) return node;
+        int countTrue = 0;
+        Node tmp = null;
         for(Node nodeM : nodeSon){
             Node res = recursiveLCAFromValue(nodeM, node1, node2);
             if(res != null){
+                tmp = res;
                 countTrue++;
-                if(res.getValue() != node1 && res.getValue() != node2) return node;
             }
-            if(countTrue>2) return node;
+            if(countTrue>=2) return node;
+        }
+        if(countTrue == 1){
+            if(tmp.getValue() != node1 && tmp.getValue() != node2) return tmp;
+            if(tmp.getValue() == node1 || tmp.getValue() == node2) return node;
         }
         return null;
     }
